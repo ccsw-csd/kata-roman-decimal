86c2e58fb51c4083486ac235a6d63f00efcb4c9e
Cristian <cristian.hurtado-sanjuan@capgemini.com>
2022-05-27 12:05:08 +0200

    Loop - Roman to Decimal

--- KataRomanNumbersLoopProject/src/main/java/com/ccsw/kata/KataRoman.java
+++ KataRomanNumbersLoopProject/src/main/java/com/ccsw/kata/KataRoman.java
@@ -1,5 +1,8 @@
 package com.ccsw.kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Main Roman Kata Class
  *
@@ -10,7 +13,31 @@ public class KataRoman {
 
   public Integer kataRomanToDecimal(String numberRoman) {
 
-    return null;
+    Map<Character, Integer> numbersMap = new HashMap<Character, Integer>();
+    numbersMap.put('I', 1);
+    numbersMap.put('V', 5);
+    numbersMap.put('X', 10);
+    numbersMap.put('L', 50);
+    numbersMap.put('C', 100);
+    numbersMap.put('D', 500);
+    numbersMap.put('M', 1000);
+
+    int result = 0;
+
+    for (int i = 0; i < numberRoman.length(); i++) {
+      char ch = numberRoman.charAt(i); // Current Roman Character
+
+      // Case 1
+      if (i > 0 && numbersMap.get(ch) > numbersMap.get(numberRoman.charAt(i - 1))) {
+        result += numbersMap.get(ch) - 2 * numbersMap.get(numberRoman.charAt(i - 1));
+      }
+
+      // Case 2: just add the corresponding number to result.
+      else
+        result += numbersMap.get(ch);
+    }
+
+    return result;
 
   }
 
