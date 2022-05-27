dd87bf30dac70392e1b16a6e1c0015d44374be7b
Cristian <cristian.hurtado-sanjuan@capgemini.com>
2022-05-27 11:24:13 +0200

    Void project

---
+++ KataRomanNumbersVoidProject/src/main/java/com/capgemini/kata/KataRoman.java
@@ -0,0 +1,11 @@
+package com.capgemini.kata;
+
+/**
+ * Main Roman Kata Class
+ *
+ * @author capgemini
+ *
+ */
+public class KataRoman {
+
+}
---
+++ KataRomanNumbersVoidProject/src/test/java/com/capgemini/kata/unit/KataRomanTest.java
@@ -0,0 +1,11 @@
+package com.capgemini.kata.unit;
+
+/**
+ * Kata Roman Test Class
+ *
+ * @author capgemini
+ *
+ */
+public class KataRomanTest {
+
+}
