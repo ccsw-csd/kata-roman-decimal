60273a0b14b1a9b9d691ce4e58863d098d570028
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2022-06-07 13:03:47 +0200

    añadido codigo de sesion

--- code/kata-sesion/src/main/java/com/ccsw/kata/RomanConversor.java
+++ code/kata-sesion/src/main/java/com/ccsw/kata/RomanConversor.java
@@ -1,5 +1,7 @@
 package com.ccsw.kata;
 
+import java.util.Hashtable;
+
 /**
  * Main Roman Kata Class
  *
@@ -8,12 +10,39 @@ package com.ccsw.kata;
  */
 public class RomanConversor {
 
+    private Hashtable<Character, Integer> mappingRomanNumeral;
+
     public RomanConversor() {
+        mappingRomanNumeral = new Hashtable<Character, Integer>();
+        mappingRomanNumeral.put('I', 1);
+        mappingRomanNumeral.put('V', 5);
+        mappingRomanNumeral.put('X', 10);
+        mappingRomanNumeral.put('L', 50);
+        mappingRomanNumeral.put('C', 100);
+        mappingRomanNumeral.put('D', 500);
+        mappingRomanNumeral.put('M', 1000);
 
     }
 
     public int convert(String romanNumeral) {
-        return 0;
+        int totalSum = 0;
+        int actualNumber;
+
+        for (int index = 0; index < romanNumeral.length(); index++) {
+            Character romanNumber = romanNumeral.charAt(index);
+            actualNumber = mappingRomanNumeral.get(romanNumber);
+
+            if (hasNextElement(romanNumeral, index) && actualNumber < mappingRomanNumeral.get(romanNumeral.charAt(index + 1)).intValue())
+                totalSum -= actualNumber;
+            else
+                totalSum += actualNumber;
+        }
+
+        return totalSum;
+    }
+
+    private boolean hasNextElement(String romanNumeral, int index) {
+        return index + 1 < romanNumeral.length();
     }
 
 }
--- code/kata-sesion/src/test/java/com/ccsw/kata/unit/RomanConversorTest.java
+++ code/kata-sesion/src/test/java/com/ccsw/kata/unit/RomanConversorTest.java
@@ -1,5 +1,12 @@
 package com.ccsw.kata.unit;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ccsw.kata.RomanConversor;
+
 /**
  * Kata Roman Test Class
  *
@@ -7,5 +14,42 @@ package com.ccsw.kata.unit;
  *
  */
 public class RomanConversorTest {
+
+    private RomanConversor romanConversor;
+
+    @Before
+    public void setUp() {
+        this.romanConversor = new RomanConversor();
+    }
+
+    @Test
+    public void convertSendIThenReturnOne() {
+        assertEquals(1, romanConversor.convert("I"));
+    }
+
+    @Test
+    public void convertSendIIThenReturnTwo() {
+        assertEquals(2, romanConversor.convert("II"));
+    }
+
+    @Test
+    public void testConvertSendIII_OK() {
+        assertEquals(3, romanConversor.convert("III"));
+    }
+
+    @Test
+    public void testConvertSendIV_OK() {
+        assertEquals(4, romanConversor.convert("IV"));
+    }
+
+    @Test
+    public void testConvertSendVIII_OK() {
+        assertEquals(8, romanConversor.convert("VIII"));
+    }
+
+    @Test
+    public void testConvertSendMCMXC_OK() {
+        assertEquals(1990, romanConversor.convert("MCMXC"));
+    }
 
 }
